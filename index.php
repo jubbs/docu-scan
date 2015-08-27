c5c0a7312f6c136bc098a2374b705c1a3d256d66
Justin Harrison <justin@hha.co.nz>
2015-08-27 12:28:57 +1200

    Push from codiad.

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	require_once('system/util.php');
 	require_once('system/config.php');
 	
-	$pageTitle = 'DocuScan';
+	$alphabet = Config::get('ALPHABET');
+	$pageTitle = 'DocuScan '.$alphabet[Config::get('COMPUTER_ID')];
 	$pageContent = $_SERVER['REQUEST_METHOD'] == 'POST' ? "views/scan.php" : "views/main.php"; // If post from main page redirect to scan page
 	include("layouts/default.php");
--- push_code.bat
+++ push_code.bat
@@ -1,5 +1,5 @@
-@ECHO off
 cd /d E:\htdocs\code\workspace\docu-scan
 git add .
 git commit -m "Push from codiad."
 git push origin master
+echo "Done"
